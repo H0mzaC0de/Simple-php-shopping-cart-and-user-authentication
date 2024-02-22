85412d1e2832fa3c8c9733c6e81c0041e05b68c2
Hamza Ait Aissa <hamzaaitaissa8@gmail.com>
2024-02-23 00:09:36 +0100

    test

--- Views/Products.php
+++ Views/Products.php
@@ -2,7 +2,7 @@
 session_start();
 if (!isset($_SESSION["user"])) {
     header("Location: login.php");
-}
+}  
 if (!empty($_SESSION['user'])) {
     require_once '../PHP/dbh.php';
     $userId = $_SESSION['user'];
